b3cf3c1f6447c62b66bca658cdef5c8047e6cac0
Riyaz1 <riyazhy168@gmail.com>
2019-02-27 00:34:58 +0530

    test

---
+++ avintech/src/main/java/com/selenium/avintech/AvintechApplication.java
@@ -0,0 +1,13 @@
+package com.selenium.avintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvintechApplication.class, args);
+	}
+
+}
